524c1370a4b0e1273eadf24784e95698354fab90
Nicolas Nueca <nicolasnueca@nicolass-air.lan>
2023-07-10 01:04:01 -0700

    wrapping router

--- src/App.js
+++ src/App.js
@@ -1,64 +1,57 @@
-import { useState, useRef } from 'react'
+import { Fragment, useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
-import { DataContext } from './context/DataContext'
-import { SearchContext } from './context/SearchContext'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 
 function App() {
-  // let [search, setSearch] = useState('')
+  let [search, setSearch] = useState('')
   let [message, setMessage] = useState('Search for Music!')
   let [data, setData] = useState([])
-  let searchInput = useRef('')
 
   const API_URL = 'https://itunes.apple.com/search?term='
 
-  // useEffect(() => {
-  //   if (search) {
-  //     const fetchData = async () => {
-  //       document.title = `${search} Music`
-  //       const response = await fetch(API_URL + search)
-  //       const resData = await response.json()
-  //       if (resData.results.length > 0) {
-  //         return setData(resData.results)
-  //       } else {
-  //         return setMessage('No Results Found')
-  //       }
-  //     }
-  //     fetchData()
-  //   }
-  // }, [search])
-  console.log('OUR STATE!!!!', data)
+  useEffect(() => {
+    if (search) {
+      const fetchData = async () => {
+        document.title = `${search} Music`
+        const response = await fetch(API_URL + search)
+        const resData = await response.json()
+        if (resData.results.length > 0) {
+          return setData(resData.results)
+        } else {
+          return setMessage('Not Found')
+        }
+      }
+      fetchData()
+    }
+  }, [search])
+
   const handleSearch = (e, term) => {
     e.preventDefault()
-    const fetchData = async () => {
-      document.title = `${term} Music`
-      const response = await fetch(API_URL + term)
-      const resData = await response.json()
-      if (resData.results.length > 0) {
-        return setData(resData.results)
-      } else {
-        return setMessage('No Results Found')
-      }
-    }
-    fetchData()
+    setSearch(term)
   }
-  console.log('DATA FROM API', data)
-  return (
-    <div className="App">
-      <SearchContext.Provider value = {{
-        term: searchInput,
-        handleSearch: handleSearch
-      }}>
-        <SearchBar />
-      </SearchContext.Provider>
-      {message}
-      <DataContext.Provider value={data}>
-        <Gallery />
-      </DataContext.Provider>
-    </div>
-  )
 
+  return (
+    <div>
+      {message}
+      <Router>
+        <Routes>
+          <Route path='/' element={
+            <Fragment>
+              <SearchBar handleSearch={handleSearch} />
+              <Gallery data={data} />
+            </Fragment>
+          } />
+          <Route path='/album/:id' element={<AlbumView />} />
+          <Route path='/artist/:id' element={<ArtistView/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,20 @@
-import { useContext } from 'react'
-import { SearchContext } from '../context/SearchContext'
-function SearchBar() {
-    const {term, handleSearch} = useContext(SearchContext)
+import { useState } from 'react'
 
-        return (
+function SearchBar(props){
+    // We can comment out our searchTerm state variable as we are not using it!
+    // let [searchTerm, setSearchTerm] = useState('')
+
+    return (
             <form>
-                <input ref={term} type="text" placeholder="SEARCH HERE" />
-                <button onClick={(e) => handleSearch(e, term.current.value)}>Submit</button>
+                <input type="text" placeholder="Search Here"
+                    onChange={
+                        (e) => props.handleSearch(e, e.target.value)
+                    } />
+                <input type="submit" />
             </form>
-        )
+    )
 }
 
 export default SearchBar
+
     
